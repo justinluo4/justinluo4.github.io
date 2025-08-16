42757d21950277f1d2b87b18f7c9f3fde607acb7
justinluo4 <justinluo4@gmail.com>
2025-08-15 21:24:00 -0700

    A

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
   },
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
+      test: /\.glsl|vs|fs|vert|frag$/,
       use: ['raw-loader', 'glslify-loader'],
     });
     return config;
